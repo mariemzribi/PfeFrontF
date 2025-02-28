b9b1b865a91a527e1650c58abf31a4d7152cac65
mariem <mariemzribi999@gmail.com>
2025-02-28 20:30:08 +0100

    new commit

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http:HttpClient) { }
+  baseURL = 'https://localhost:7104/api';
+
+  createUser(formData:any){
+    return this.http.post(this.baseURL+'/register',formData);
+  }
+  
+}
